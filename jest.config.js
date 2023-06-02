ccf525f0749856a10d72bd108a8641b1da4a03ef
Darlley Brito <darlleybrito@gmail.com>
2023-06-02 17:36:35 -0300

    update

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testEnvironment: "jsdom",
+    testePathIgnorePatterns: [
+        '/node_modules/',
+        '/.next/'
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: ['/src/**/*.ts(x)'],
+    setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,8 @@
+const Main = () => {
+	return (
+		<main>
+			<h1>react avançado</h1>
+		</main>
+	)
+}
+export default Main
