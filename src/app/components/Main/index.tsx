b2e2838867ae4661abb197a19804f6ec571809e0
Darlley <systembrtt@gmail.com>
2023-10-27 09:09:07 -0400

    replace styles and stories

--- src/app/components/Main/index.tsx
+++ src/app/components/Main/index.tsx
@@ -1,9 +1,21 @@
-import * as Style from './styles'
+import * as S from './styles'
 
-export default function Main() {
+export default function Main({
+  title = 'React Avançado',
+  description = 'Typecript, ReactS, NextsJS e Styled Components'
+}) {
   return (
-    <Style.Wrapper>
-      <h1>React Avançado</h1>
-    </Style.Wrapper>
+    <S.Wrapper>
+      <S.Logo
+        src="./logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Headline>{title}</S.Headline>
+      <S.Description>{description}</S.Description>
+      <S.Ilustration
+        src="./hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código."
+      />
+    </S.Wrapper>
   )
 }
